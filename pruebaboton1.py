3397a17b6faf29252e1e4aaf809b11fc3d92c4ab
Simon <111585746+sixmon55@users.noreply.github.com>
2025-06-08 19:24:12 -0300

    Update pruebaboton1.py

--- pruebaboton1.py
+++ pruebaboton1.py
@@ -48,7 +48,6 @@ def pantalla_arrepentimiento():
             if id_venta not in ventas_dict:
                 estado = v[4]
                 fecha_estado = v[3]
-                # 🔽 Eliminado: no modificar artificialmente el estado según la fecha
                 ventas_dict[id_venta] = {
                     "email": v[1],
                     "id_destino": v[2],
